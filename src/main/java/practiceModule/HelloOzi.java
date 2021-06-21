7a53800edb076189256e6bed6fd9639c8f1b6fcd
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-21 18:21:02 +0100

    an error was detected

--- src/main/java/practiceModule/HelloOzi.java
+++ src/main/java/practiceModule/HelloOzi.java
@@ -5,6 +5,6 @@ public class HelloOzi {
 	public static void main(String[] args){
 		
 		System.out.print("OZI HAVE SEEN AND ACCEPTED YOUR GIT HUB INVITE.\n");
-		System.out.print("So we can get started here?");
+		System.out.print("So we can get started here!");
 	}
 }
