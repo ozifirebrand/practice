c528963ac40c82871c5c1f0f750b279532b32c19
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-06-21 18:17:58 +0100

    comment was updated

--- src/com/myown/Main.java
+++ src/com/myown/Main.java
@@ -6,6 +6,16 @@ public class Main {
         System.out.print("Hello,");
         System.out.println("this is my first Java program");
 	// write your code here
+ 
+//        README
+
+
+//        So in these block of code you have  a package called "com.myown" which is the best naming covention
+//        for naming a package... And the class name is too is top-nouch..
+//        Any ways you have no problem here... My problem is just that "Please push your codes to git"
+    
+    
+    
     }
 }
 
